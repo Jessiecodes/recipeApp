377ed3ae7c63b59649fba53947c7a5dc1849d62a
Jessiecodes <Jessak1202@gmail.com>
2020-04-03 08:13:17 -0400

    Updates

---
+++ precache-manifest.6923715716888c9f06ee5754b5c172b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d9712a40c679ca030cd11a3c2de72e",
+    "url": "/recipeApp/index.html"
+  },
+  {
+    "revision": "8c2f3e8ed54270a0b3f4",
+    "url": "/recipeApp/static/css/main.13015305.chunk.css"
+  },
+  {
+    "revision": "485392cd05a23b881067",
+    "url": "/recipeApp/static/js/2.ecad9cd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeApp/static/js/2.ecad9cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2f3e8ed54270a0b3f4",
+    "url": "/recipeApp/static/js/main.55bc5917.chunk.js"
+  },
+  {
+    "revision": "6e0a306e9d5fe3d423dd",
+    "url": "/recipeApp/static/js/runtime-main.427b83f3.js"
+  }
+]);
